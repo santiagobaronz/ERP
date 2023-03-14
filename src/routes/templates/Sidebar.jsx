fde0d6e4de982824de065752764a54252a6c33f7
Santiago Baron Zuleta <santiagobazul@hotmail.com>
2023-03-14 12:35:15 -0500

    [Cambio de estilos] Menú se mantiene en modo oscuro

--- src/routes/templates/Sidebar.jsx
+++ src/routes/templates/Sidebar.jsx
@@ -30,43 +30,43 @@ export const Sidebar = () => {
 	}
 
 	return (
-		<div className='flex flex-col w-72 min-h-screen bg-bg-white-secondary dark:bg-bg-dark-secondary p-6 pt-8'>
+		<div className='flex flex-col w-72 min-h-screen bg-bg-dark-secondary p-6 pt-8'>
 			<div className='flex gap-x-4 items-center pb-5'>
-				<img src="/assets/logo-icon-white.png" alt="Logo de la empresa" className='w-14 p-3 dark:bg-black bg-gray-white rounded-xl' />
+				<img src="/assets/logo-icon-white.png" alt="Logo de la empresa" className='w-14 p-3 bg-black rounded-xl' />
 				<div>
-					<p className='text-black dark:text-white font-medium'>{settings.NOMBRE_EMPRESA}</p>
-					<p className='capitalize dark:text-gray-dark text-gray-white text-sm'>{auth().NOMBRES.toLowerCase()}</p>
+					<p className=' text-white font-medium'>{settings.NOMBRE_EMPRESA}</p>
+					<p className='capitalize text-gray-dark text-sm'>{auth().NOMBRES.toLowerCase()}</p>
 				</div>
 			</div>
 			<div className='flex flex-col justify-between flex-grow mt-5'>
 				<ul>
 					<div>
-						<h2 className='text-gray-white dark:text-gray-dark text-sm font-medium mb-2 select-none'>Información personal</h2>
+						<h2 className='text-gray-dark text-sm font-medium mb-2 select-none'>Información personal</h2>
 
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+							<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<IoIosStats className='text-base' />
 								<p className='ml-4 text-sm'>Mis estadísticas</p>
 							</Link>
 						</li>
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<RxPerson className='text-base' />
 								<p className='ml-4 text-sm'>Mi perfil</p>
 							</Link>
 						</li>
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<RxLayers className='text-base' />
 								<p className='ml-4 text-sm'>Mis pagos</p>
 							</Link>
 						</li>
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<RxMixerHorizontal className='text-base' />
 								<p className='ml-4 text-sm'>Configuración</p>
 							</Link>
@@ -77,43 +77,43 @@ export const Sidebar = () => {
 						<h2 className='text-gray-white dark:text-gray-dark text-sm font-medium mb-2 select-none'>Gestión de información</h2>
 
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<IoIosStats className='text-base' />
 								<p className='ml-4 text-sm'>Estadisticas</p>
 							</Link>
 						</li>
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<BsFillBoxSeamFill className='text-base' />
 								<p className='ml-4 text-sm'>Inventario</p>
 							</Link>
 						</li>
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<BsCurrencyDollar className='text-base' />
 								<p className='ml-4 text-sm'>Ventas</p>
 							</Link>
 						</li>
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<BsListUl className='text-base' />
 								<p className='ml-4 text-sm'>Categorias</p>
 							</Link>
 						</li>
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<BsFillBasket3Fill className='text-base' />
 								<p className='ml-4 text-sm'>Productos</p>
 							</Link>
 						</li>
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<BsFillPersonVcardFill className='text-base' />
 								<p className='ml-4 text-sm'>Clientes</p>
 							</Link>
@@ -124,15 +124,15 @@ export const Sidebar = () => {
 						<h2 className='text-gray-white dark:text-gray-dark text-sm font-medium mb-2 select-none'>Control de proveedores</h2>
 
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<BsFillPersonLinesFill className='text-base' />
 								<p className='ml-4 text-sm'>Proveedores</p>
 							</Link>
 						</li>
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<BsViewList className='text-base' />
 								<p className='ml-4 text-sm'>Lista de pedidos</p>
 							</Link>
@@ -143,54 +143,50 @@ export const Sidebar = () => {
 						<h2 className='text-gray-white dark:text-gray-dark text-sm font-medium mb-2 select-none'>Gestión administrativa</h2>
 
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<BsPeopleFill className='text-base' />
 								<p className='ml-4 text-sm'>Control de empleados</p>
 							</Link>
 						</li>
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<BsCashCoin className='text-base' />
 								<p className='ml-4 text-sm'>Gestión de nomina</p>
 							</Link>
 						</li>
 						<li className='mb-1'>
-							<Link className={`flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black 
-								${location.pathname === '/#' ? 'dark:text-white text-black dark:bg-bg-dark-hover bg-bg-white-hover' : 'dark:text-gray-dark text-gray-white'}`}>
+						<Link className={`flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover hover:text-white
+								${location.pathname === '/#' ? 'text-white bg-bg-dark-hover' : 'text-gray-dark '}`}>
 								<RxGear className='text-base' />
 								<p className='ml-4 text-sm'>Ajustes generales</p>
 							</Link>
 						</li>
 					</div>
-
-
-
-
-
-
 				</ul>
+
 				<ul className='mt-20'>
 					<li className='mb-1' onClick={handleToggleColor}>
-						<Link className='flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black dark:text-gray-dark text-gray-white'>
+						<Link className='flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover  hover:text-white   text-gray-white'>
 							<IoIosColorPalette className='text-base' />
 							<p className='ml-4 text-sm'>Modo {darkMode ? 'claro' : 'oscuro'}</p>
 						</Link>
 					</li>
 					<li className='mb-1'>
-						<Link className='flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black dark:text-gray-dark text-gray-white'>
+						<Link className='flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover  hover:text-white   text-gray-white'>
 							<RxQuestionMarkCircled className='text-base' />
 							<p className='ml-4 text-sm'>Centro de ayuda</p>
 						</Link>
 					</li>
 					<li className='mb-1' onClick={() => signOut()}>
-						<Link className='flex items-center h-10 p-3 font-medium rounded-md dark:hover:bg-bg-dark-hover hover:bg-bg-white-hover dark:hover:text-white hover:text-black dark:text-gray-dark text-gray-white'>
+						<Link className='flex items-center h-10 p-3 font-medium rounded-md hover:bg-bg-dark-hover  hover:text-white   text-gray-white'>
 							<RxExit className='text-base' />
 							<p className='ml-4 text-sm'>Cerrar sesión</p>
 						</Link>
 					</li>
 				</ul>
+				
 			</div>
 		</div>
 	)
